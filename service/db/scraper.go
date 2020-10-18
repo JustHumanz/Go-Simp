895df005be4bb070242ce55b17a4dc6023df2f36
JustHumanz <Aldinsetiawan99@gmail.com>
2020-10-19 00:49:53 +0700

    fix yt nill

--- service/db/scraper.go
+++ service/db/scraper.go
@@ -170,7 +170,7 @@ func (Data Member) GetYtSubs() []Subs {
 		}
 		return datasubs
 	} else {
-		datasubs[0] = Subs{}
+		datasubs = append(datasubs, Subs{})
 		datasubs[0].Data.Subscribers = 0
 		datasubs[0].Data.Videos = 0
 		datasubs[0].Data.Views = 0
