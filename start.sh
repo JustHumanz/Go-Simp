5d0480f119a77c03a7797cea094133ae2919d56e
JustHumanz <Aldinsetiawan99@gmail.com>
2020-10-21 22:22:23 +0700

    update new vtuber

--- start.sh
+++ start.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
-nohup tor &
-go run .
+
+for (( ; ; ))
+do
+    if ping -c 1 db_migrate &> /dev/null
+    then
+        echo "Still waiting db_migrate"
+    else
+        exit 0
+    fi
+    sleep 1
+done
