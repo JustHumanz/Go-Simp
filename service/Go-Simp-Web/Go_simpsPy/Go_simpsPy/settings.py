764035fbb55742a55559c5e75377fe7e1ac1dec1
JustHumanz <Aldinsetiawan99@gmail.com>
2020-11-07 23:00:30 +0700

    Update go_simp_web

--- service/Go-Simp-Web/Go_simpsPy/Go_simpsPy/settings.py
+++ service/Go-Simp-Web/Go_simpsPy/Go_simpsPy/settings.py
@@ -120,3 +120,23 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': '../bang_jago.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
