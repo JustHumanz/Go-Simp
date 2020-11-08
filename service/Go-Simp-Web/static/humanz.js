1385ff0ff58f8fdd3f570d21372eb14517779bbf
JustHumanz <Aldinsetiawan99@gmail.com>
2020-11-08 20:08:30 +0700

    fix member img

--- service/Go-Simp-Web/static/humanz.js
+++ service/Go-Simp-Web/static/humanz.js
@@ -83,60 +83,33 @@ function Del(elem) {
 });
 
 
-! function(a) {
-  a.fn.isOnScreen = function(b) {
-      var c = this.outerHeight(),
-          d = this.outerWidth();
-      if (!d || !c) return !1;
-      var e = a(window),
-          f = {
-              top: e.scrollTop(),
-              left: e.scrollLeft()
-          };
-      f.right = f.left + e.width(), f.bottom = f.top + e.height();
-      var g = this.offset();
-      g.right = g.left + d, g.bottom = g.top + c;
-      var h = {
-          top: f.bottom - g.top,
-          left: f.right - g.left,
-          bottom: g.bottom - f.top,
-          right: g.right - f.left
-      };
-      return "function" == typeof b ? b.call(this, h) : h.top > 0 && h.left > 0 && h.right > 0 && h.bottom > 0
-  }
-}(jQuery);
+const counters = document.querySelectorAll('.counter');
+const speed = 200; // The lower the slower
 
-$(document).ready(function() {
-  checkDisplay();
+counters.forEach(counter => {
+const updateCount = () => {
+const target = +counter.getAttribute('data-target');
+const count = +counter.innerText;
 
-$(window).on('resize scroll', function() {
-  checkDisplay();
-});
-});
+// Lower inc to slow and higher to slow
+const inc = target / speed;
 
-function checkDisplay(){
-$('.prg-count').each(function() {
-    var $this = $(this);
-    if ($this.isOnScreen()) {
-      var countTo = $this.attr('data-count');
-      $({
-        countNum: $this.text()
-      }).animate({
-        countNum: countTo
-      }, {
-        duration: 8000,
-        easing: 'linear',
-        step: function() {
-          $this.text(Math.floor(this.countNum));
-        },
-        complete: function() {
-          $this.text(this.countNum);
-          //alert('finished');
-        }
-      });
-    }
-  });
+// console.log(inc);
+// console.log(count);
+
+// Check if target is reached
+if (count < target) {
+// Add inc to count and output in counter
+counter.innerText = count + inc;
+// Call function every ms
+setTimeout(updateCount, 1);
+} else {
+counter.innerText = target;
 }
+};
+
+updateCount();
+});
 
 function IsEmpty() {
   if (document.forms['form'].Nickname.value === "" || document.forms['form'].Region.value === "") {
