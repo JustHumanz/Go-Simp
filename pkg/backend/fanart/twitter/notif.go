5ceebe177c2b28cd7ce90a6e40fac808f2887114
JustHumanz <Aldinsetiawan99@gmail.com>
2020-11-28 03:15:39 +0700

    add Saki-chan #39

--- pkg/backend/fanart/twitter/notif.go
+++ pkg/backend/fanart/twitter/notif.go
@@ -48,7 +48,16 @@ func (Data PushData) SendNude() error {
 		go func(DiscordChannel string, wg *sync.WaitGroup) {
 			defer wg.Done()
 			Color, _ = engine.GetColor("/tmp/tw", Data.Image)
-			var tags string
+			var (
+				tags      string
+				GroupIcon string
+			)
+
+			if match, _ := regexp.MatchString("404.jpg", Data.Group.GroupIcon); match {
+				GroupIcon = ""
+			} else {
+				GroupIcon = Data.Group.GroupIcon
+			}
 			if URLTMP != url {
 				if UserTagsList != nil {
 					tags = strings.Join(UserTagsList, " ")
@@ -59,7 +68,7 @@ func (Data PushData) SendNude() error {
 
 				} else {
 					msg, err := Bot.ChannelMessageSendEmbed(DiscordChannel, engine.NewEmbed().
-						SetAuthor(strings.Title(Data.Group.GroupName), Data.Group.GroupIcon).
+						SetAuthor(strings.Title(Data.Group.GroupName), GroupIcon).
 						SetTitle(Data.UserName+"(@"+Data.ScreenName+")").
 						SetURL(url).
 						SetThumbnail(strings.Replace(Data.Avatar, "_normal.jpg", ".jpg", -1)).
