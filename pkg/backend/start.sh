41698878221320625618fdc9df72f2d4df3de3ce
JustHumanz <Aldinsetiawan99@gmail.com>
2020-12-22 21:19:07 +0700

    update start script

--- pkg/backend/start.sh
+++ pkg/backend/start.sh
@@ -1,13 +1,32 @@
 #!/bin/sh
 
-while true
-do
-    if ping -c 1 db_migrate &> /dev/null
-    then
-        echo "Still waiting db_migrate"
-    else
-        echo "db_migrate done,let's fvcking goooooo!!!!!"
-        break
+BuildModule() {
+    go build -o backend
+}
+
+RunModule(){
+    ./backend
+    exit_status=$?
+    if [ $exit_status -eq 1 ]; then
+        exit $exit_status
     fi
-    sleep 30
-done
+    exit $exit_status
+}
+
+
+Start(){
+    BuildModule
+    while true
+    do
+        if ping -c 1 db_migrate &> /dev/null
+        then
+            echo "Still waiting db_migrate"
+        else
+            echo "db_migrate done,let's fvcking goooooo!!!!!"
+            RunModule
+        fi
+        sleep 30
+    done
+}
+
+Start
