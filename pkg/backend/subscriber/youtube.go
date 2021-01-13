2412d7e9d1a0673849648836e298bafcc5e33067
JustHumanz <Aldinsetiawan99@gmail.com>
2021-01-13 20:27:37 +0700

    fix yt SubscriberCount

--- pkg/backend/subscriber/youtube.go
+++ pkg/backend/subscriber/youtube.go
@@ -3,8 +3,6 @@ package main
 import (
 	"encoding/json"
 	"strconv"
-	"strings"
-	"time"
 
 	config "github.com/JustHumanz/Go-simp/tools/config"
 	database "github.com/JustHumanz/Go-simp/tools/database"
@@ -17,110 +15,96 @@ func CheckYoutube() {
 	var YTstate Subs
 	Token := engine.GetYtToken()
 	for _, Group := range engine.GroupData {
-		var VtubChannel []string
-		Names := database.GetMembers(Group.ID)
-		for i, Member := range Names {
-			if Member.YoutubeID != "" {
-				VtubChannel = append(VtubChannel, Member.YoutubeID)
+		for _, Member := range database.GetMembers(Group.ID) {
+			body, err := network.Curl("https://www.googleapis.com/youtube/v3/channels?part=statistics&id="+Member.YoutubeID+"&key="+Token, nil)
+			if err != nil {
+				log.Error(err, string(body))
 			}
-
-			if i == 24 || i == len(Names)-1 {
-				body, err := network.Curl("https://www.googleapis.com/youtube/v3/channels?part=statistics&id="+strings.Join(VtubChannel, ",")+"&key="+Token, nil)
-				if err != nil {
-					log.Error(err, string(body))
-					break
-				}
-				err = json.Unmarshal(body, &YTstate)
-				if err != nil {
-					log.Error(err)
-				}
-				for _, Name2 := range Names {
-					for _, Item := range YTstate.Items {
-						if Name2.YoutubeID == Item.ID && !Item.Statistics.HiddenSubscriberCount {
-							YtSubsDB, err := Name2.GetSubsCount()
-							if err != nil {
-								log.Error(err)
+			err = json.Unmarshal(body, &YTstate)
+			if err != nil {
+				log.Error(err)
+			}
+			for _, Item := range YTstate.Items {
+				if Member.YoutubeID == Item.ID && !Item.Statistics.HiddenSubscriberCount {
+					YtSubsDB, err := Member.GetSubsCount()
+					if err != nil {
+						log.Error(err)
+					}
+					YTSubscriberCount, err := strconv.Atoi(Item.Statistics.SubscriberCount)
+					if err != nil {
+						log.Error(err)
+					}
+					SendNotif := func(SubsCount string) {
+						Color, err := engine.GetColor(config.TmpDir, Member.YoutubeAvatar)
+						if err != nil {
+							log.Error(err)
+						}
+						VideoCount, err := strconv.Atoi(Item.Statistics.VideoCount)
+						if err != nil {
+							log.Error(err)
+						}
+						ViewCount, err := strconv.Atoi(Item.Statistics.ViewCount)
+						if err != nil {
+							log.Error(err)
+						}
+						SendNude(engine.NewEmbed().
+							SetAuthor(Group.GroupName, Group.IconURL, "https://www.youtube.com/channel/"+Member.YoutubeID+"?sub_confirmation=1").
+							SetTitle(engine.FixName(Member.EnName, Member.JpName)).
+							SetThumbnail(config.YoutubeIMG).
+							SetDescription("Congratulation for "+SubsCount+" subscriber").
+							SetImage(Member.YoutubeAvatar).
+							AddField("Viewers", engine.NearestThousandFormat(float64(ViewCount))).
+							AddField("Videos", engine.NearestThousandFormat(float64(VideoCount))).
+							InlineAllFields().
+							SetURL("https://www.youtube.com/channel/"+Member.YoutubeID+"?sub_confirmation=1").
+							SetColor(Color).MessageEmbed, Group, Member.ID)
+					}
+					if YtSubsDB.YtSubs != YTSubscriberCount {
+						if YTSubscriberCount >= 1000000 {
+							for i := 0; i < 10000001; i += 1000000 {
+								if i == YTSubscriberCount && !Item.Statistics.HiddenSubscriberCount {
+									SendNotif(engine.NearestThousandFormat(float64(i)))
+								}
 							}
-							YTSubscriberCount, err := strconv.Atoi(Item.Statistics.SubscriberCount)
-							if err != nil {
-								log.Error(err)
+						} else if YTSubscriberCount >= 100000 {
+							for i := 0; i < 1000001; i += 100000 {
+								if i == YTSubscriberCount && !Item.Statistics.HiddenSubscriberCount {
+									SendNotif(engine.NearestThousandFormat(float64(i)))
+								}
 							}
-							SendNotif := func(SubsCount string) {
-								Color, err := engine.GetColor(config.TmpDir, Name2.YoutubeAvatar)
-								if err != nil {
-									log.Error(err)
+						} else if YTSubscriberCount >= 10000 {
+							for i := 0; i < 100001; i += 10000 {
+								if i == YTSubscriberCount && !Item.Statistics.HiddenSubscriberCount {
+									SendNotif(engine.NearestThousandFormat(float64(i)))
 								}
-								VideoCount, err := strconv.Atoi(Item.Statistics.VideoCount)
-								if err != nil {
-									log.Error(err)
-								}
-								ViewCount, err := strconv.Atoi(Item.Statistics.ViewCount)
-								if err != nil {
-									log.Error(err)
-								}
-								SendNude(engine.NewEmbed().
-									SetAuthor(Group.GroupName, Group.IconURL, "https://www.youtube.com/channel/"+Name2.YoutubeID+"?sub_confirmation=1").
-									SetTitle(engine.FixName(Name2.EnName, Name2.JpName)).
-									SetThumbnail(config.YoutubeIMG).
-									SetDescription("Congratulation for "+SubsCount+" subscriber").
-									SetImage(Name2.YoutubeAvatar).
-									AddField("Viewers", engine.NearestThousandFormat(float64(ViewCount))).
-									AddField("Videos", engine.NearestThousandFormat(float64(VideoCount))).
-									InlineAllFields().
-									SetURL("https://www.youtube.com/channel/"+Name2.YoutubeID+"?sub_confirmation=1").
-									SetColor(Color).MessageEmbed, Group, Name2.ID)
 							}
-							if YtSubsDB.YtSubs != YTSubscriberCount {
-								if YTSubscriberCount >= 1000000 {
-									for i := 0; i < 10000001; i += 1000000 {
-										if i == YTSubscriberCount && !Item.Statistics.HiddenSubscriberCount {
-											SendNotif(engine.NearestThousandFormat(float64(i)))
-										}
-									}
-								} else if YTSubscriberCount >= 100000 {
-									for i := 0; i < 1000001; i += 100000 {
-										if i == YTSubscriberCount && !Item.Statistics.HiddenSubscriberCount {
-											SendNotif(engine.NearestThousandFormat(float64(i)))
-										}
-									}
-								} else if YTSubscriberCount >= 10000 {
-									for i := 0; i < 100001; i += 10000 {
-										if i == YTSubscriberCount && !Item.Statistics.HiddenSubscriberCount {
-											SendNotif(engine.NearestThousandFormat(float64(i)))
-										}
-									}
-								} else if YTSubscriberCount >= 1000 {
-									for i := 0; i < 10001; i += 1000 {
-										if i == YTSubscriberCount && !Item.Statistics.HiddenSubscriberCount {
-											SendNotif(engine.NearestThousandFormat(float64(i)))
-										}
-									}
+						} else if YTSubscriberCount >= 1000 {
+							for i := 0; i < 10001; i += 1000 {
+								if i == YTSubscriberCount && !Item.Statistics.HiddenSubscriberCount {
+									SendNotif(engine.NearestThousandFormat(float64(i)))
 								}
-
-								log.WithFields(log.Fields{
-									"Past Youtube subscriber":    YtSubsDB.YtSubs,
-									"Current Youtube subscriber": YTSubscriberCount,
-									"Vtuber":                     Name2.EnName,
-								}).Info("Update Youtube subscriber")
-								VideoCount, err := strconv.Atoi(Item.Statistics.VideoCount)
-								if err != nil {
-									log.Error(err)
-								}
-								ViewCount, err := strconv.Atoi(Item.Statistics.ViewCount)
-								if err != nil {
-									log.Error(err)
-								}
-								YtSubsDB.UpYtSubs(YTSubscriberCount).
-									UpYtVideo(VideoCount).
-									UpYtViews(ViewCount).
-									UpdateSubs("yt")
 							}
 						}
+
+						log.WithFields(log.Fields{
+							"Past Youtube subscriber":    YtSubsDB.YtSubs,
+							"Current Youtube subscriber": YTSubscriberCount,
+							"Vtuber":                     Member.EnName,
+						}).Info("Update Youtube subscriber")
+						VideoCount, err := strconv.Atoi(Item.Statistics.VideoCount)
+						if err != nil {
+							log.Error(err)
+						}
+						ViewCount, err := strconv.Atoi(Item.Statistics.ViewCount)
+						if err != nil {
+							log.Error(err)
+						}
+						YtSubsDB.UpYtSubs(YTSubscriberCount).
+							UpYtVideo(VideoCount).
+							UpYtViews(ViewCount).
+							UpdateSubs("yt")
 					}
 				}
-			}
-			if i%10 == 0 {
-				time.Sleep(3 * time.Second)
 			}
 		}
 	}
