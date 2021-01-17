61113d1c6a6e4c9239c4624bce02a38540279f62
JustHumanz <Aldinsetiawan99@gmail.com>
2021-01-17 18:18:20 +0700

    Add Brave group

--- service/migrate/db.go
+++ service/migrate/db.go
@@ -392,7 +392,7 @@ func AddData(Data Vtuber) {
 	var (
 		GroupIDIndependen        int64
 		GroupName                = "Independen"
-		GroupIcon                = "https://cdn.human-z.tech/404.jpg"
+		GroupIcon                = "https://cdn." + config.BotConf.Domain + "/404.jpg"
 		NewVtuberNamesIndependen []string
 	)
 	row := db.QueryRow("SELECT id FROM VtuberGroup WHERE VtuberGroupName=?", GroupName)
