ed98a1f5a1ba70e3641c574d7e109639bdc3a685
JustHumanz <Aldinsetiawan99@gmail.com>
2021-02-07 01:26:26 +0700

    Recovery utility

---
+++ service/utility/runfunc/run.go
@@ -0,0 +1,33 @@
+package runfunc
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+	log "github.com/sirupsen/logrus"
+)
+
+func Run(Bot *discordgo.Session) {
+	shutdown := make(chan int)
+	//create a notification channel to shutdown
+	sigChan := make(chan os.Signal, 1)
+
+	//register for interupt (Ctrl+C) and SIGTERM (docker)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-sigChan
+		log.Info("Shutting down...")
+		if Bot != nil {
+			log.Info("Close bot wss session")
+			err := Bot.Close()
+			if err != nil {
+				log.Error(err)
+			}
+		}
+		shutdown <- 1
+	}()
+
+	<-shutdown
+}
