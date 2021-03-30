f5e7d39356a0b935b83bb8aad24b6e539be78aff
JustHumanz <Aldinsetiawan99@Gmail.com>
2021-03-30 22:52:48 +0700

    Fix live bilibili not send & space bilibili vtuber name

--- service/livestream/bilibili/space/check.go
+++ service/livestream/bilibili/space/check.go
@@ -41,21 +41,7 @@ func CheckSpace(Data *database.LiveStream, limit string) {
 		Data.AddVideoID(video.Bvid).SetType(Videotype).
 			UpdateTitle(video.Title).
 			UpdateThumbnail("https:" + video.Pic).UpdateSchdule(time.Unix(int64(video.Created), 0).In(loc)).
-			UpdateViewers(strconv.Itoa(video.Play)).UpdateLength(video.Length)
-		/*
-			Data = database.LiveStream{
-				VideoID: video.Bvid,
-				Type:    Videotype,
-				Title:   video.Title,
-				Thumb:   "https:" + video.Pic,
-				Desc:    video.Description,
-				Schedul: time.Unix(int64(video.Created), 0).In(loc),
-				Viewers: strconv.Itoa(video.Play),
-				Length:  video.Length,
-				Member:  Data.Member,
-				Group:   Data.Group,
-			}
-		*/
+			UpdateViewers(strconv.Itoa(video.Play)).UpdateLength(video.Length).SetState(config.SpaceBili)
 		new, id := Data.CheckVideo()
 		if new {
 			log.WithFields(log.Fields{
