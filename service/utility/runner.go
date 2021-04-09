fe35fba965e428c55bad66179466524b21777f1b
JustHumanz <Aldinsetiawan99@Gmail.com>
2021-04-09 22:09:52 +0700

    Update donate message

--- service/utility/runner.go
+++ service/utility/runner.go
@@ -163,7 +163,7 @@ func main() {
 				AddField("Ko-Fi", "[Link]("+Donation+")").
 				AddField("if you a broke gang,you can upvote "+BotInfo.Username, "[Top.gg]("+configfile.TopGG+")").
 				InlineAllFields().
-				AddField("or give some star on github", "[Github](https://github.com/JustHumanz/Go-Simp)").MessageEmbed)
+				AddField("or listen some music from dev waifu (kano/鹿乃)", "[鹿乃チャンネルofficial](https://www.youtube.com/watch?v=pCa_oSjBU1A&list=PLBQuo9fQ-4eMX8TSweBQhoduIPt6wEnHI&index=15)\nHope you like her voice ❤️").MessageEmbed)
 		})
 	}
 	c.AddFunc(config.CheckServerCount, func() {
