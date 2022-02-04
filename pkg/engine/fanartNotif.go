74e077d3deea3f796ef0e005f51a01052d1e2f3c
Humanz <just@humanz.moe>
2022-02-04 17:26:02 +0700

    send fanart notif with async

--- pkg/engine/fanartNotif.go
+++ pkg/engine/fanartNotif.go
@@ -2,6 +2,7 @@ package engine
 
 import (
 	"strings"
+	"sync"
 
 	"github.com/JustHumanz/Go-Simp/pkg/config"
 	database "github.com/JustHumanz/Go-Simp/pkg/database"
@@ -17,6 +18,8 @@ func SendFanArtNude(Art database.DataFanart, Bot *discordgo.Session, Color int) 
 			var (
 				ChannelData []database.DiscordChannel
 				err1        error
+				wg          sync.WaitGroup
+				counter     int
 			)
 			if Art.Lewd {
 				ChannelData, err1 = database.ChannelTag(Member.ID, 1, config.LewdChannel, Member.Region)
@@ -39,36 +42,50 @@ func SendFanArtNude(Art database.DataFanart, Bot *discordgo.Session, Color int) 
 				icon = config.BiliBiliIMG
 			}
 
-			for _, Channel := range ChannelData {
-				if Art.Group.GroupName == config.Indie && !Channel.IndieNotif {
-					//do nothing,like my life
-					continue
-				} else {
-					tmp, err := Bot.ChannelMessageSendEmbed(Channel.ChannelID, NewEmbed().
-						SetAuthor(strings.Title(Art.Group.GroupName), Art.Group.IconURL).
-						SetTitle(Art.Author).
-						SetURL(Art.PermanentURL).
-						SetThumbnail(Art.AuthorAvatar).
-						SetDescription(Art.Text).
-						SetImage(Art.Photos...).
-						SetFooter(Art.State, icon).
-						InlineAllFields().
-						SetColor(Color).MessageEmbed)
-					if err != nil {
-						log.Error(tmp, err.Error())
-						err = Channel.DelChannel(err.Error())
+			for _, C := range ChannelData {
+				wg.Add(1)
+
+				if counter%50 == 0 {
+					log.WithFields(log.Fields{
+						"Func":  "Fanart",
+						"Value": counter,
+					}).Warn("Waiting send message")
+					wg.Wait()
+				}
+				go func(Channel database.DiscordChannel, wg *sync.WaitGroup) {
+					defer wg.Done()
+					if Art.Group.GroupName == config.Indie && !Channel.IndieNotif {
+						//do nothing,like my life
+						return
+					} else {
+						tmp, err := Bot.ChannelMessageSendEmbed(Channel.ChannelID, NewEmbed().
+							SetAuthor(strings.Title(Art.Group.GroupName), Art.Group.IconURL).
+							SetTitle(Art.Author).
+							SetURL(Art.PermanentURL).
+							SetThumbnail(Art.AuthorAvatar).
+							SetDescription(Art.Text).
+							SetImage(Art.Photos...).
+							SetFooter(Art.State, icon).
+							InlineAllFields().
+							SetColor(Color).MessageEmbed)
+						if err != nil {
+							log.Error(tmp, err.Error())
+							err = Channel.DelChannel(err.Error())
+							if err != nil {
+								log.Error(err)
+							}
+						}
+						err = Reacting(map[string]string{
+							"ChannelID": Channel.ChannelID,
+						}, Bot)
 						if err != nil {
 							log.Error(err)
 						}
 					}
-					err = Reacting(map[string]string{
-						"ChannelID": Channel.ChannelID,
-					}, Bot)
-					if err != nil {
-						log.Error(err)
-					}
-				}
+				}(C, &wg)
+				counter++
 			}
+			wg.Wait()
 		}
 	}
 }
