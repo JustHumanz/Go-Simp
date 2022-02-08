093b043741987384966cc84ea1521cc26c05f87f
Ian Ardiansyah <ian.ian.wold76@gmail.com>
2022-02-08 16:04:48 +0700

    rollback for commit poblem

---
+++ service/new-web-vue/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router"
+import HelloWorld from "@/components/HelloWorld.vue"
+import "./index.css"
+</script>
+
+<template>
+  <header>
+    <nav></nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style></style>
