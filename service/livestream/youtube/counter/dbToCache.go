4bdedf21cab700051a9224b6f81c1558a224371b
Humanz <just@humanz.moe>
2022-03-09 01:47:31 +0700

    fix yt-counter cacheChecker

--- service/livestream/youtube/counter/dbToCache.go
+++ service/livestream/youtube/counter/dbToCache.go
@@ -6,32 +6,34 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-//CacheChcker send upcoming livestream from db to cache
-func CacheChcker() {
+//CacheChecker send upcoming livestream from db to cache
+func CacheChecker() {
 	log.Info("Start check upcoming from db")
 	liveCache, err := database.GetUpcomingFromCache()
 	if err != nil {
 		log.Error(err)
 	}
-	for _, v := range *GroupPayload {
-		liveStream, err := v.GetYtLiveStream(config.UpcomingStatus, nil)
-		if err != nil {
-			log.Error(err)
-		}
-		for _, k := range liveStream {
-			for _, j := range liveCache {
-				j := j.(database.LiveStream)
-				if k.VideoID == j.VideoID {
-					continue
-				} else {
-					log.WithFields(log.Fields{
-						"videoID": j.VideoID,
-						"agency":  v.GroupName,
-						"vtuber":  k.Member.Name,
-					}).Info("Found upcoming on db but not in cache,send it to cache now")
-					err := k.SendToCache(false)
-					if err != nil {
-						log.Error(err)
+	for _, Agency := range *GroupPayload {
+		for _, Member := range Agency.Members {
+			liveStream, err := Member.GetYtLiveStream(config.UpcomingStatus)
+			if err != nil {
+				log.Error(err)
+			}
+			for _, k := range liveStream {
+				for _, j := range liveCache {
+					j := j.(database.LiveStream)
+					if k.VideoID == j.VideoID {
+						continue
+					} else {
+						log.WithFields(log.Fields{
+							"videoID": j.VideoID,
+							"agency":  Agency.GroupName,
+							"vtuber":  k.Member.Name,
+						}).Info("Found upcoming on db but not in cache,send it to cache now")
+						err := k.SendToCache(false)
+						if err != nil {
+							log.Error(err)
+						}
 					}
 				}
 			}
