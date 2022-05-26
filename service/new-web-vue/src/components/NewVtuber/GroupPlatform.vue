1ffcc0b229a122afaf5bb7dddac0614f3bd6b24e
yarndinasti <ian.ian.wold76@gmail.com>
2022-05-27 02:17:06 +0700

    dark mode color for group form

--- service/new-web-vue/src/components/NewVtuber/GroupPlatform.vue
+++ service/new-web-vue/src/components/NewVtuber/GroupPlatform.vue
@@ -301,7 +301,7 @@ export default {
 <style lang="scss" scoped>
 .platform {
   &-group {
-    @apply mb-2 rounded-lg bg-slate-200 md:ml-2;
+    @apply mb-2 rounded-lg bg-slate-200 dark:bg-slate-400 md:ml-2;
 
     &.show {
       .arrow {
@@ -313,10 +313,10 @@ export default {
     }
 
     &.errors {
-      @apply bg-red-200;
+      @apply bg-red-200 dark:bg-red-400;
 
       .platform-link {
-        @apply bg-red-300;
+        @apply bg-red-300 dark:bg-red-500;
       }
     }
 
@@ -329,7 +329,7 @@ export default {
 
         &.has-error {
           input {
-            @apply bg-red-400;
+            @apply bg-red-400 dark:bg-red-700;
           }
 
           .description {
@@ -346,11 +346,11 @@ export default {
         }
 
         select {
-          @apply my-1 -translate-y-0.5 rounded-lg bg-slate-100 p-2 shadow-md transition duration-200 ease-in-out hover:translate-y-0 hover:shadow-sm focus:translate-y-0.5 focus:shadow-none;
+          @apply my-1 -translate-y-0.5 rounded-lg bg-slate-100 p-2 shadow-md transition duration-200 ease-in-out hover:translate-y-0 hover:shadow-sm focus:translate-y-0.5 focus:shadow-none dark:bg-slate-500;
         }
 
         input {
-          @apply my-1 -translate-y-0.5 rounded-lg bg-slate-100 p-2 shadow-md transition duration-200 ease-in-out hover:translate-y-0 hover:shadow-sm focus:translate-y-0.5 focus:shadow-none focus:outline-none;
+          @apply my-1 -translate-y-0.5 rounded-lg bg-slate-100 p-2 shadow-md transition duration-200 ease-in-out hover:translate-y-0 hover:shadow-sm focus:translate-y-0.5 focus:shadow-none focus:outline-none dark:bg-slate-500;
         }
 
         small {
@@ -358,7 +358,7 @@ export default {
         }
 
         .description {
-          @apply text-gray-600;
+          @apply text-gray-600 dark:text-slate-50;
         }
 
         .error {
@@ -369,7 +369,7 @@ export default {
   }
 
   &-link {
-    @apply flex items-center justify-between rounded-lg bg-slate-300 px-4 py-2 shadow-md;
+    @apply flex items-center justify-between rounded-lg bg-slate-300 px-4 py-2 shadow-md dark:bg-slate-600;
 
     &__icon {
       @apply flex items-center space-x-4;
@@ -382,7 +382,7 @@ export default {
 }
 
 .delete-platform {
-  @apply rounded-full bg-white p-1 px-1.5;
+  @apply rounded-full bg-white p-1 px-1.5 transition duration-200 ease-in-out dark:text-slate-700;
 
   &.confirm {
     @apply bg-red-500 text-white;
