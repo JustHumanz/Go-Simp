337ca6d8d4def1498a3b0b3ebb79fe1a052649fb
yarndinasti <ian.ian.wold76@gmail.com>
2022-05-29 06:37:57 +0700

    Impement state management

---
+++ service/new-web-vue/src/stores/groups.js
@@ -0,0 +1,9 @@
+import { defineStore } from "pinia"
+import { ref, computed } from "vue"
+import axios from "axios"
+
+export const useGroupStore = defineStore("groups", () => {
+  const groups = []
+
+  const fetchGroups = async () => {}
+})
