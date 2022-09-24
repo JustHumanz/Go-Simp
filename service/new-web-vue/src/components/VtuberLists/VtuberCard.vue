6f410a5ae4251eb59363aa1744bd2228930ec16c
yarndinasti <ian.ian.wold76@gmail.com>
2022-09-24 08:58:03 +0700

    add vituber card

---
+++ service/new-web-vue/src/components/VtuberLists/VtuberCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="vtuber-cards m-5 w-44">
+    <img src="/assets/smolame.jpg" alt="" class="vtuber-cards-cover__images" />
+    Name
+  </div>
+</template>
+<script></script>
+<style lang="scss" scoped>
+.vtuber-cards {
+  @apply overflow-hidden rounded-md bg-red-500 transition-all duration-300 ease-in-out hover:rounded-lg;
+
+  &-cover {
+    &__images {
+      @apply aspect-square w-full object-contain;
+    }
+  }
+}
+</style>
